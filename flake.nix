b98f98af58b502b767b5475eb0818a2d6b665cc7
davidsielert <456592+davidsielert@users.noreply.github.com>
2024-11-27 23:18:22 -0800

    --amend

--- flake.nix
+++ flake.nix
@@ -35,24 +35,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # NixVim input, following `nixpkgs`
-    #nixvim = {
-    #  url = "github:nix-community/nixvim";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-    khanelivim = {
-      url = "github:khaneliman/khanelivim";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    Neve = {
-      url = "github:redyf/Neve";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     mac-app-util.url = "github:hraban/mac-app-util";
+    systems.url = "github:nix-systems/default-darwin";
 
     nvf.url = "github:notashelf/nvf";
   };
@@ -61,66 +47,64 @@
   # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
   # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
   # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
-  outputs = {
-    self,
-    nixpkgs,
-    darwin,
-    home-manager,
-    khanelivim,
-    Neve,
-    nvf,
-    ...
-  } @ inputs: let
-    # User-specific settings
-    username = "davidsielert";
-    useremail = "david@sielert.com";
-    system = "aarch64-darwin"; # Use "aarch64-darwin" for Apple Silicon, "x86_64-darwin" for Intel Macs
-    hostname = "mbp14";
-    myOverlays = self: super: {
-      # Adding eslint to the package set
-      eslint = super.nodePackages.eslint or null;
-    };
-    sources = import ./npins;
-    # Package set for the selected system
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [myOverlays];
-    };
-    # Special arguments passed to modules
-    specialArgs =
-      inputs
-      // {
-        inherit
-          username
-          useremail
-          hostname
-          inputs
-          nvf
-          ;
-      };
-  in {
-    darwinConfigurations."${hostname}" = darwin.lib.darwinSystem {
-      inherit system specialArgs;
-      modules = [
-        ./modules/nix-core.nix
-        ./modules/system.nix
-        ./modules/apps.nix
-        ./modules/host-users.nix
-        # nixvim.nixDarwinModules.nixvim
-        # Home Manager integration
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = specialArgs;
-          home-manager.backupFileExtension = "before-nix";
-          home-manager.users.${username} = import ./home;
-        }
-        # ./modules/nixvim/nixvim.nix
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        ./args.nix
       ];
-    };
+      systems = import inputs.systems;
+      flake = let
+        # User-specific settings
+        username = "davidsielert";
+        useremail = "david@sielert.com";
+        system = "aarch64-darwin"; # Use "aarch64-darwin" for Apple Silicon, "x86_64-darwin" for Intel Macs
+        hostname = "mbp14";
+        myOverlays = self: super: {
+          # Adding eslint to the package set
+          eslint = super.nodePackages.eslint or null;
+        };
+        # Package set for the selected system
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [myOverlays];
+        };
+        nvf = inputs.nvf;
+        # Special arguments passed to modules
+        specialArgs =
+          inputs
+          // {
+            inherit
+              username
+              useremail
+              hostname
+              inputs
+              nvf
+              ;
+          };
+      in {
+        darwinConfigurations."${hostname}" = inputs.darwin.lib.darwinSystem {
+          inherit system specialArgs;
+          modules = [
+            ./modules/nix-core.nix
+            ./modules/system.nix
+            ./modules/apps.nix
+            ./modules/host-users.nix
+            # nixvim.nixDarwinModules.nixvim
+            # Home Manager integration
+            inputs.home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = specialArgs;
+              home-manager.backupFileExtension = "before-nix";
+              home-manager.users.${username} = import ./home;
+            }
+            # ./modules/nixvim/nixvim.nix
+          ];
+        };
 
-    # Nix code formatter
-    formatter.${system} = pkgs.alejandra;
-  };
+        # Nix code formatter
+        formatter.${system} = pkgs.alejandra;
+      };
+    };
 }
